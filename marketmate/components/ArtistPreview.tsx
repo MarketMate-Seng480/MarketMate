0f778e08c4d37c80ef72f68a0c5b1ba86804e57c
jenny <jluu@uvic.ca>
2024-02-13 03:19:08 +0000

    image rendering for carousel

--- marketmate/components/ArtistPreview.tsx
+++ marketmate/components/ArtistPreview.tsx
@@ -1,12 +1,13 @@
 'use client'
 import React, { useState } from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Stack, HStack, VStack } from '@chakra-ui/react'
 
 interface ArtistPreviewProps {
+    shopLogo: string;
     images: string[];
 }
 
-const ArtistPreview: React.FC<ArtistPreviewProps> = ({ images }) => {
+const ArtistPreview: React.FC<ArtistPreviewProps> = ({ shopLogo, images }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const handleNext = () => {
@@ -19,15 +20,27 @@ const ArtistPreview: React.FC<ArtistPreviewProps> = ({ images }) => {
 
     return (
         <div>
-
+            <img src={shopLogo} alt="Shop Logo" />
             <div>
-                
-                {/* <Button colorScheme='teal' variant='solid' onClick={handlePrev}>Previous</Button> */}
-                <button onClick={handlePrev}>Previous</button>
-                <img src={images[currentImageIndex]} alt="Carousel Image" />
-                <button onClick={handleNext}>Next</button>
+                    {images.length > 3 && 
+                    <HStack>
+                        {currentImageIndex > 0 && <Button colorScheme='teal' variant='solid' onClick={handlePrev}>Previous</Button>}
+                        <img src={images[currentImageIndex]} alt="Carousel Image" />
+                        <img src={images[currentImageIndex + 1]} alt="Carousel Image" />
+                        <img src={images[currentImageIndex + 2]} alt="Carousel Image" />
+                        {currentImageIndex < (images.length - 3) && <Button colorScheme='teal' variant='solid' onClick={handleNext}>Next</Button>}
+                    </HStack>   
+                    }
+
+                    {images.length <=3 && 
+                    <HStack>
+                        <img src={images[currentImageIndex]} alt="Carousel Image" />
+                        <img src={images[currentImageIndex + 1]} alt="Carousel Image" />
+                        <img src={images[currentImageIndex + 2]} alt="Carousel Image" />
+                    </HStack>
+                    }
             </div>
-            {/* <Button colorScheme='blue'>Button</Button> */}
+            <Button colorScheme='blue'>Button</Button>
         </div>
     );
 };
--- marketmate/src/app/page.tsx
+++ marketmate/src/app/page.tsx
@@ -1,11 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import ArtistPreview from "../../components/ArtistPreview"
 
 
 function Home() {
     return (
-    <div>
-        <span>This is the homepage</span>
-        {/* <Carousel images={["https://via.placeholder.com/150", "https://via.placeholder.com/150", "https://via.placeholder.com/150"]}/> */}
-    </div>
+        <ChakraProvider>
+            <div>
+            <span>This is the homepage</span>
+            <ArtistPreview shopLogo={"https://via.placeholder.com/100"} images={["https://via.placeholder.com/100", "https://via.placeholder.com/110", "https://via.placeholder.com/120", "https://via.placeholder.com/130", "https://via.placeholder.com/140", "https://via.placeholder.com/150", "https://via.placeholder.com/160"]}/>
+            {/* <ArtistPreview images={["https://via.placeholder.com/130", "https://via.placeholder.com/140", "https://via.placeholder.com/150"]}/> */}
+        </div>
+        </ChakraProvider>
     )
 }
  
