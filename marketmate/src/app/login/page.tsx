7751c9e69b551f44ccee7800b9fc36423198a8c0
julhoang <64108232+julhoang@users.noreply.github.com>
2024-02-14 20:10:14 +0000

    begin setting up auth

--- marketmate/src/app/login/page.tsx
+++ marketmate/src/app/login/page.tsx
@@ -1,19 +1,38 @@
-'use client'
-import React from 'react';
-import { Button, Link, Box } from '@chakra-ui/react';
-import { useAuth } from '../authContext';
+"use client";
+import React from "react";
+import { Button, Link, Box, Flex, Img, Center, Heading } from "@chakra-ui/react";
+import { useAuth } from "../authContext";
 
 // Switches user state to isVendor (logged in state)
 const Login: React.FC = () => {
   const { login } = useAuth();
   return (
-    <Box display='flex' flexDirection='column' gap='1rem'>
-      Login page
-      <Button onClick={login} width='fit-content'>
-        <Link href='/vendor'>Proceed as vendor</Link>
-      </Button>
-    </Box>
+    <Flex>
+      <Box>
+        <Img
+          src="https://via.placeholder.com/150"
+          alt="logo"
+        />
+      </Box>
+      <Box>
+        <Center>
+          <Heading
+            as="h3"
+            size="lg"
+          >
+            Login
+          </Heading>
+        </Center>
+      </Box>
+    </Flex>
   );
-}
+};
 
 export default Login;
+
+// <Box display='flex' flexDirection='column' gap='1rem'>
+//       Login page
+//       <Button onClick={login} width='fit-content'>
+//         <Link href='/vendor'>Proceed as vendor</Link>
+//       </Button>
+//     </Box>
--- marketmate/src/app/page.tsx
+++ marketmate/src/app/page.tsx
@@ -1,7 +1,42 @@
-function Home() {
+import React from "react";
+import { Box, Heading, Img, Center } from "@chakra-ui/react";
+import { AuthProvider, useAuth } from "./authContext";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const Home: React.FC = () => {
+  const { isVendor } = useAuth();
   return (
-      <span>This is the homepage</span>
-  )
-}
- 
-export default Home
+    <ChakraProvider>
+      <AuthProvider>
+        <Box>
+          <Center>
+            <Heading
+              as="h1"
+              size="xl"
+            >
+              Welcome to MarketMate
+            </Heading>
+          </Center>
+          <Center>
+            <Img
+              src="https://via.placeholder.com/150"
+              alt="logo"
+            />
+          </Center>
+        </Box>
+        <Box>
+          <Center>
+            <Heading
+              as="h3"
+              size="lg"
+            >
+              {isVendor ? "Vendor" : "Customer"} Dashboard
+            </Heading>
+          </Center>
+        </Box>
+      </AuthProvider>
+    </ChakraProvider>
+  );
+};
+
+export default Home;
