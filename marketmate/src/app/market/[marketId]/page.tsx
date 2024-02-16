e65c06d915b0de20b39f5855ed6bda2994db653c
maddiesolis <madelinesdeo@gmail.com>
2024-02-16 00:26:06 +0000

    market page renders different info based on slug

--- marketmate/src/app/market/[marketId]/page.tsx
+++ marketmate/src/app/market/[marketId]/page.tsx
@@ -1,9 +1,28 @@
+'use client'
+import Navbar from "@/app/components/Navbar";
+import { Box, Center, Heading } from "@chakra-ui/react";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { sampleMarkets } from "@/app/page";
 
 export default function MarketPage() {
+    // Temporarily hardcoded, will actually be an API request
+    const path = usePathname();
+    const slug = path.split("/").pop();
+    const market = sampleMarkets.find(market => market.marketId === slug);
     return (
-        <div>
-            market page
-        </div>
+        <>
+        <Navbar />
+        <Box>
+          <Center>
+            <Heading
+              as="h1"
+              size="xl"
+            >
+              {market?.title ?? "Market Not Found"}
+            </Heading>
+          </Center>
+        </Box>
+      </>
     )
 }
