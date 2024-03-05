b52305edb0040de2fffbe6aaaac7b2ffa3dcd178
Jon <JonCote95@gmail.com>
2024-03-05 12:21:49 -0800

    public.user entry created on supabase auth.user creation, user api endpoints added, and api fixes to now work with id as uuid instead of integer

--- marketmate/src/app/api/users/[id]/route.ts
+++ marketmate/src/app/api/users/[id]/route.ts
@@ -3,28 +3,28 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Get a specific vendor
+// Get a specific user
 export async function GET(req: Request, { params: {id} }: { params: { id: string } }) {
-    const vendor = await prisma.vendor.findUnique({
+    const user = await prisma.user.findUnique({
         where: {
             id
         }
     })
-    if (!vendor) return NextResponse.json({ 
-        error: "Error vendor not found",
+    if (!user) return NextResponse.json({ 
+        error: "Error user not found",
         status: 500 
     })
         
-    return NextResponse.json({ message: 'ok', status: 200, data: vendor })
+    return NextResponse.json({ message: 'ok', status: 200, data: user })
 }
 
 
-// Patch for updating a vendor
+// Patch for updating a user
 export async function PATCH(req: Request, { params: {id} }: { params: { id: string } }) {
     // TODO: add auth check here to require user to be logged in and be the vendor before updating a vendor profile
 
     const json = await req.json()
-    const updated = await prisma.vendor.update({
+    const updated = await prisma.user.update({
         where: {
             id
         },
@@ -35,11 +35,11 @@ export async function PATCH(req: Request, { params: {id} }: { params: { id: stri
 
 
 
-// Delete a vendor
+// Delete a user
 export async function DELETE(req: Request, { params: {id} }: { params: { id: string } }) {
     // TODO: add auth check here to require user to be logged in and be the vendor before updating a vendor profile
 
-    const deleted = await prisma.vendor.delete({
+    const deleted = await prisma.user.delete({
         where: {
             id
         }
