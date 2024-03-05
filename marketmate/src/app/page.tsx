64c7b9f0717c2537b7c572fecde66084e280a55d
julhoang <64108232+julhoang@users.noreply.github.com>
2024-03-05 07:41:58 +0000

    Finished vendor carousel

--- marketmate/src/app/page.tsx
+++ marketmate/src/app/page.tsx
@@ -1,57 +1,92 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { Box, Heading, Center, Stack, Text } from "@chakra-ui/react";
-
-import { UpcomingEvents } from "@components/UpcomingEvents";
-import HomepageSection from "@components/LocalArtisans";
+import {
+  Box,
+  Heading,
+  Center,
+  Stack,
+  Text,
+  Flex,
+  Image,
+  Avatar,
+  Button,
+  VStack,
+  SimpleGrid,
+  Spacer,
+} from "@chakra-ui/react";
+import VendorCarousel from "@components/VendorCarousel";
 import { PageContainer } from "@components/PageContainer";
-import { MultiArtistsPreviews } from "@components/MultiArtistPreviews";
 
-import { sampleMarkets } from "./sampleData/sampleMarkets";
-import { sampleVendors } from "./sampleData/sampleVendors";
+function HeroSection() {
+  return (
+    <Flex
+      w="100%"
+      h={{
+        base: "40vh",
+        lg: "50vh",
+      }}
+      bgGradient="linear(to-r, #CA7A6C, #575F90)"
+      color="white"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Heading
+        size={{ base: "md", md: "lg" }}
+        px={20}
+        textAlign={"center"}
+      >
+        Connecting local art makers to local art buyers
+      </Heading>
+    </Flex>
+  );
+}
 
+function FeatureVendorSection() {
+  return (
+    <Flex
+      bg="white"
+      px={10}
+      w="full"
+      alignContent={"center"}
+      justifyContent={"center"}
+      direction={"column"}
+    >
+      <VStack spacing={8}>
+        <VStack spacing={2}>
+          <Heading
+            as="h4"
+            fontSize={"32"}
+          >
+            Featured Vendors
+          </Heading>
+          <Text size={"xs"}>Discover Victoria&apos;s local artisans</Text>
+        </VStack>
 
+        <VendorCarousel />
 
+        <Button variant={"outline"}>Discover More Vendors</Button>
+      </VStack>
+    </Flex>
+  );
+}
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const user = await supabase.auth.getUser();
 
-
   return (
     <PageContainer>
       <Center flexDirection={"column"}>
-        <Box my={75}>
-          <Heading
-            as="h1"
-            size="xl"
-          >
-            Welcome to MarketMate
-          </Heading>
-
-          <Text color={"green"}>User ID: {user?.data.user?.id || "Not logged in"}</Text>
-
-          <Heading
-            as="h1"
-            size="md"
-            color="grey"
-            textAlign="center"
-          >
-            Connect with Victoria&apos;s local artisans
-          </Heading>
-        </Box>
-
-        <Stack
-          direction={"column"}
-          spacing={15}
+        <VStack
+          spacing={{ base: 8, md: 12 }}
+          w="full"
         >
-          <HomepageSection title="Upcoming Events">
-            <UpcomingEvents events={sampleMarkets} />
-          </HomepageSection>
-          <HomepageSection title="Local Artisans">
-            <MultiArtistsPreviews vendors={sampleVendors} />
-          </HomepageSection>
-        </Stack>
+          <HeroSection />
+
+          <FeatureVendorSection />
+
+          <Spacer />
+        </VStack>
       </Center>
     </PageContainer>
   );
--- marketmate/src/middleware.ts
+++ marketmate/src/middleware.ts
@@ -30,5 +30,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/vendor/:path"],
+  matcher: ["/", "/vendor/:path*"],
 };
