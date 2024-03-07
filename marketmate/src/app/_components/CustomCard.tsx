d0ad5fc0d56308d4b5007c71eeedd6339db80bbc
maddiesolis <madelinesdeo@gmail.com>
2024-03-07 22:49:41 +0000

    removed old navbar

--- marketmate/src/app/_components/CustomCard.tsx
+++ marketmate/src/app/_components/CustomCard.tsx
@@ -9,11 +9,15 @@ export const CustomCard: React.FC<CustomCardProps> = ({ children, ...rest }) => 
     const borderColor = useTheme().colors.gray[500];
     return (
         <Box
+            display={'flex'}
+            flexDirection={'column'}
             border={'1px'}
             borderColor={borderColor}
             rounded={"md"}
             boxShadow={"md"}
             overflow={"hidden"}
+            padding={'5'}
+            gap={'5'}
             {...rest}
         >
             {children}
