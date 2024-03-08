58fb15aa3f83b8698059a449a4ab0295feba575e
julhoang <64108232+julhoang@users.noreply.github.com>
2024-03-08 01:05:35 +0000

    Finish batch email to different vendors

--- marketmate/src/app/_components/VendorConfirmationMail.tsx
+++ marketmate/src/app/_components/VendorConfirmationMail.tsx
@@ -1,72 +1,40 @@
-import { Box, Button, Center, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
+import { OrderInfo } from "../confirmation/page";
 
-interface VendorEmailProps {
+export interface VendorEmailProps {
   vendorName: string;
   buyerName: string;
   buyerEmail: string;
-  productLists: string[];
+  products: OrderInfo[];
 }
 
 const VendorEmailTemplate: React.FC<VendorEmailProps> = ({
   vendorName,
   buyerName,
   buyerEmail,
-  productLists,
+  products,
 }) => {
   return (
-    <Box
-      bg="white"
-      w="100%"
-      p={4}
-      borderRadius="md"
-      boxShadow="0 0 10px rgba(0, 0, 0, 0.1)"
-      border="1px solid rgba(0, 0, 0, 0.1)"
-    >
-      <Center>
-        <Image
-          src="./ArtiswayLogo.png" // Add the actual URL or local path for the vendor's logo
-          alt={`${vendorName} Logo`}
-          w="100px"
-        />
-      </Center>
-      <Text
-        fontSize="xl"
-        fontWeight="bold"
-        mt={4}
-      >
-        Hello {vendorName}!
-      </Text>
-      <Text
-        fontSize="lg"
-        mt={4}
-      >
-        Exciting news! {buyerName} is interested in your product{productLists.length > 1 ? "s" : ""}
-        :
-      </Text>
+    <div>
+      Hello {vendorName}!
+      <br />
+      <br />
+      Exciting news! {buyerName} is interested in your product{products.length > 1 ? "s" : ""}:
       <ul>
-        {productLists.map((product, index) => (
-          <li key={index}>{product}</li>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.productName} - Quantity: {product.quantity}
+          </li>
         ))}
       </ul>
-      <Text
-        fontSize="lg"
-        mt={4}
-      >
-        {buyerName} can&apos;t wait to hear from you. Contact them at {buyerEmail}.
-      </Text>
-      <Link
-        href={`mailto:${buyerEmail}`}
-        isExternal
-      >
-        <Button
-          mt={4}
-          colorScheme="teal"
-        >
-          Contact {buyerName}
-        </Button>
-      </Link>
-    </Box>
+      Next Steps: {buyerName} can&apos;t wait to hear from you. Contact them at {buyerEmail} to
+      arrange <b>order pickups</b> and <b>payments</b>.
+      <br />
+      <br />
+      Best,
+      <br />
+      The Artisway Team
+    </div>
   );
 };
 
--- marketmate/src/app/api/emails/route.ts
+++ marketmate/src/app/api/emails/route.ts
@@ -1,42 +1,79 @@
-import { NextResponse, NextRequest } from "next/server";
-import VendorEmailTemplate from "@/app/_components/VendorConfirmationMail";
-import { Resend } from "resend";
 import React from "react";
-import { supabase } from "@/app/lib/supabase";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { NextResponse, NextRequest } from "next/server";
+import { Resend } from "resend";
+import VendorEmailTemplate from "@/app/_components/VendorConfirmationMail";
+import BuyerEmailTemplate from "@/app/_components/BuyerConfirmationEmail";
+import { OrderInfo, EmailProps } from "@/app/confirmation/page";
 
 const resend = new Resend(process.env.RESEND_API);
 
-export async function POST(request: NextRequest) {
-  const supabase = createServerComponentClient({ cookies });
-  const buyer = await supabase.auth.getUser();
-  console.log(buyer);
+interface ResendRequest {
+  from: string;
+  to: string;
+  subject: string;
+  react: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+}
 
-  if (!buyer) {
-    return new NextResponse(JSON.stringify({ error: "User not found" }), {
-      status: 404,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+// loop through the productLists to group the products by vendor
+function groupProductsByVendor(emailProps: EmailProps): Map<string, OrderInfo[]> {
+  const vendorProductMap = new Map<string, OrderInfo[]>();
+
+  emailProps.productLists.forEach((order) => {
+    const { vendorID, productName, quantity, vendorName, vendorEmail } = order;
+
+    // Check if the vendor is already in the map
+    if (vendorProductMap.has(vendorID)) {
+      // If yes, add the product to the existing list
+      vendorProductMap
+        .get(vendorID)!
+        .push({ productName, quantity, vendorName, vendorID, vendorEmail });
+    } else {
+      // If no, create a new list for the vendor and add the product
+      vendorProductMap.set(vendorID, [
+        { productName, quantity, vendorName, vendorID, vendorEmail },
+      ]);
+    }
+  });
+
+  return vendorProductMap;
+}
+
+export async function POST(request: NextRequest) {
+  const { buyerName, buyerEmail, productLists }: EmailProps = await request.json();
+
+  const vendorProductMap = groupProductsByVendor({ buyerName, buyerEmail, productLists });
+
+  // loop through the vendorProducts to send the email to each vendor
+  const vendorsBatch: ResendRequest[] = Array.from(vendorProductMap).map(([vendorID, products]) => {
+    const { vendorEmail, vendorName } = products[0];
+
+    return {
+      from: "Artisway <onboarding@resend.dev>",
+      to: vendorEmail,
+      subject: buyerName + " is Interested in Your Unique Products! 🎉",
+      react: VendorEmailTemplate({
+        vendorName,
+        buyerName,
+        buyerEmail,
+        products,
+      }) as React.ReactElement,
+    };
+  });
+
+  const buyerEmailRequest: ResendRequest = {
+    from: "Artisway <onboarding@resend.dev>",
+    to: buyerEmail,
+    subject: "We Have Received Your Request to Order! 🎉",
+    react: BuyerEmailTemplate({
+      buyerName,
+      productLists,
+    }) as React.ReactElement,
+  };
 
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Artisway <onboarding@resend.dev>", // TODO: change this to the artisway email
-      to: [buyer.data.user?.email as string],
-      subject: "Hello world",
-      react: VendorEmailTemplate({
-        vendorName: "Lucas Store",
-        buyerName: "Julia",
-        buyerEmail: "julhoang11@gmail.com",
-        productLists: ["Product 1", "Product 2", "Product 3"],
-      }) as React.ReactElement,
-    });
+    const { data, error } = await resend.batch.send([...vendorsBatch, buyerEmailRequest]);
 
     if (error) {
-      console.error(error);
       return new NextResponse(JSON.stringify({ error }), {
         status: 500,
         headers: {
@@ -45,7 +82,6 @@ export async function POST(request: NextRequest) {
       });
     }
 
-    console.log(data);
     return new NextResponse(JSON.stringify({ data }), {
       status: 200,
       headers: {
