9c5b3f57b609f1cd7c81cc97e45520a1c18caaf1
julhoang <64108232+julhoang@users.noreply.github.com>
2024-03-08 01:12:22 +0000

    Add sample page to call email verification

--- marketmate/src/app/confirmation/page.tsx
+++ marketmate/src/app/confirmation/page.tsx
@@ -1,9 +1,64 @@
 "use client";
 import { Button } from "@chakra-ui/button";
 import React from "react";
-import type { Cart } from "@prisma/client";
+import type { Cart, Product, Vendor } from "@prisma/client";
+
+export interface OrderInfo {
+  productName: string;
+  quantity: number;
+  vendorName: string;
+  vendorID: string;
+  vendorEmail: string;
+}
+export interface EmailProps {
+  buyerName: string; // User's first_name + last_name
+  buyerEmail: string; // User's email
+  productLists: OrderInfo[]; // Array of Products
+}
 
 export default function SendVendorEmail() {
+  const buyerName = "John Doe";
+  const buyerEmail = "jj@tester.com";
+  const email1 = "sample@gmail.com";
+  const email2 = "sample2@gmail.com";
+
+  const productLists: OrderInfo[] = [
+    {
+      productName: "Product 1",
+      vendorName: "Luca Store",
+      quantity: 1,
+      vendorEmail: email2,
+      vendorID: "1",
+    },
+    {
+      productName: "Product 2",
+      vendorName: "Luca Store",
+      quantity: 1,
+      vendorEmail: email2,
+      vendorID: "1",
+    },
+    {
+      productName: "Product 3",
+      vendorName: "Julia Store",
+      quantity: 1,
+      vendorEmail: email2,
+      vendorID: "2",
+    },
+    {
+      productName: "Product 4",
+      vendorName: "Julia Store",
+      quantity: 1,
+      vendorEmail: email2,
+      vendorID: "2",
+    },
+  ];
+
+  const orderRequest: EmailProps = {
+    buyerName,
+    buyerEmail,
+    productLists,
+  };
+
   async function handleClick() {
     try {
       const response = await fetch("/api/emails", {
@@ -11,7 +66,7 @@ export default function SendVendorEmail() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({}),
+        body: JSON.stringify(orderRequest),
       });
 
       if (response.ok) {
