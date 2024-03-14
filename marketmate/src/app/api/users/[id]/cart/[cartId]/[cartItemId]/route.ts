cbc324b73d9f899c455bdbada8cd4024e0263bde
Jon <JonCote95@gmail.com>
2024-03-14 12:26:24 -0700

    fix issue where multiple cartItems where made for same cart-product instead of updating the quantity of the cartItem

--- marketmate/src/app/api/users/[id]/cart/[cartId]/[cartItemId]/route.ts
+++ marketmate/src/app/api/users/[id]/cart/[cartId]/[cartItemId]/route.ts
@@ -27,34 +27,6 @@ export async function PATCH(req: Request, { params: { id, cartId, cartItemId } }
     return NextResponse.json({ message: 'ok', status: 200, data: updated })
 }
 
-
-// post a new product to the cart
-export async function POST(req: Request, { params: { id, cartId, cartItemId } }: { params: { id: string, cartId: string, cartItemId: string } }) {
-    const json = await req.json();
-    const newItem = await prisma.cart_Item.create({
-      data: {
-        cartId: cartId,
-        productId: json.productId,
-        quantity: json.quantity,
-      },
-    });
-
-    const updated = await prisma.cart.update({
-      where: {
-        id: cartId,
-      },
-      data: {
-        cartItem: {
-          connect: {
-            id: newItem.id,
-          },
-        },
-      },
-    });
-
-    return NextResponse.json({ message: 'ok', status: 200, data: updated })
-}
-
 // delete a product from the cart
 export async function DELETE(req: Request, { params: { id, cartId, cartItemId } }: { params: { id: string, cartId: string, cartItemId: string } }) {
     const json = await req.json();
--- marketmate/src/app/api/users/[id]/cart/[cartId]/route.ts
+++ marketmate/src/app/api/users/[id]/cart/[cartId]/route.ts
@@ -30,6 +30,53 @@ export async function PATCH(req: NextRequest, { params: { id, cartId } }: { para
   return NextResponse.json({ message: "ok", status: 200, data: updated });
 }
 
+// post a new product to the cart
+export async function POST(req: Request, { params: { id, cartId } }: { params: { id: string, cartId: string } }) {
+  const json = await req.json();
+
+  const item = await prisma.cart_Item.findMany({
+    where: {
+      cartId: cartId,
+      productId: json.productId,
+    },
+  });
+  if(item[0]) {
+    const updated = await prisma.cart_Item.update({
+      where: {
+        id: item[0].id,
+      },
+      data: {
+        quantity: item[0].quantity + json.quantity,
+      },
+    });
+    return NextResponse.json({ message: 'ok', status: 200, data: updated })
+  }
+  else {
+    const newItem = await prisma.cart_Item.create({
+      data: {
+        cartId: cartId,
+        productId: json.productId,
+        quantity: json.quantity,
+      },
+    });
+
+    const updated = await prisma.cart.update({
+      where: {
+        id: cartId,
+      },
+      data: {
+        cartItem: {
+          connect: {
+            id: newItem.id,
+          },
+        },
+      },
+    });
+  }
+
+  return NextResponse.json({ message: 'ok', status: 200 })
+}
+
 
 // delete a unique cart
 export async function DELETE(req: Request, { params: { id, cartId } }: { params: { id: string, cartId: string } }) {
