79d0ce682279fd123e3341a2827f5ec7079458b5
Jon <JonCote95@gmail.com>
2024-03-22 19:05:11 -0700

    API fixes for product total not updating properly and Product addition to cart working

--- marketmate/src/app/product/[id]/page.tsx
+++ marketmate/src/app/product/[id]/page.tsx
@@ -1,67 +1,36 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { VStack, Image, Text, useTheme, SimpleGrid, useToast } from "@chakra-ui/react";
 import PageContainer from "@components/PageContainer";
 import { usePathname } from "next/navigation";
-import type { Product, Vendor } from "@prisma/client";
+import type { Product, Vendor, User } from "@prisma/client";
 import { NavLink } from "@components/navigation/CustomLinks";
 import { CustomButton } from "@components/CustomButton";
 import LoadingPage from "@components/Loading";
-import { User as AuthUser } from "@supabase/auth-helpers-nextjs";
 import { supabase } from "@/app/lib/supabase";
-import type {
-  User,
-  Product as PrismaProduct,
-  Vendor as PrismaVendor,
-  Cart as PrismaCart,
-} from "@prisma/client";
+
 
 export default function ProductPage() {
   const toast = useToast();
-  const [authUser, setAuthUser] = useState<AuthUser | null>();
-  const [user, setUser] = useState<User | null>();
   const colors = useTheme().colors;
   const path = usePathname();
   const slug = path.split("/").pop();
   const fetchURL = `/api/products/${slug}`;
-  const [cartId, setCartId] = useState<String | null>(null);
   const [product, setProduct] = useState<Product | null>(null);
   const [vendor, setVendor] = useState<Vendor | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    supabase.auth
-      .getSession()
-      .then((session) => {
-        setAuthUser(session?.data?.session?.user ?? null);
-      })
-      .catch((err) => {
-        console.log("ERROR GET SESSION: ", err);
-      });
-  }, []); // Empty dependency array ensures this effect runs only once on mount
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        if (authUser) {
-          const response = await fetch(`/api/users/${authUser.id}`);
-          const data = await response.json();
-          setUser(data.data);
-        }
-      } catch (error) {
-        console.log("Error fetching user", error);
-      }
-    };
-    
-    fetchUser();
-  }, [authUser]);
-
-  // the issue is that this fetchCart runs on mount and when user is changing
+  
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await fetch(fetchURL);
+        const response = await fetch(fetchURL, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
         const data = await response.json();
         console.log("data", data);
         setProduct(data.data);
@@ -69,16 +38,35 @@ export default function ProductPage() {
         console.error("Error fetching product:", error);
       }
     };
-
     fetchProduct();
   }, [fetchURL]);
 
   useEffect(() => {
+    const fetchUser = async () => {
+      const session = (await supabase.auth.getSession()).data.session;
+      if (!session) return;
+
+      const userData = await fetch(`/api/users/${session.user.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const userDataJson = await userData.json();
+      console.log("user data", userDataJson);
+      setUser(userDataJson.data);
+    }
+    
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
     const fetchCart = async () => {
       try{
-        if (user?.cartId != null) {
+        if (user === null) return;
+        if (user?.cartId !== null) {
             console.log("cartID is not null");
-            setCartId(user.cartId);
+           // setCartId(user.cartId);
         } else {
             console.log("No cart found for user, generating new cart");
             const newCart = await fetch(`/api/users/${user?.id}/cart`, {
@@ -88,16 +76,19 @@ export default function ProductPage() {
               },
             });
             const cartData = await newCart.json();
-            setCartId(cartData.id);
+          //  setCartId(cartData.id);
             console.log("New cart:", cartData);
         }
       } catch (error) {
         console.error("Error fetching cart data:", error); 
       }
     };
-    if (user != null) {
+
+
+    if (user !== null) {
       fetchCart();
     }
+
   }, [user]);
 
   useEffect(() => {
@@ -118,49 +109,55 @@ export default function ProductPage() {
     }
   }, [product]);
 
+
   useEffect(() => {
-      if (cartId != null) {
-          setIsLoading(false);
+      if(product !== null) {
+        setIsLoading(false);
       }
-  }, [cartId]);
+
+  }, [product]);
 
 
   async function addToCart() {
-      setIsLoading(true);
-      console.log("Cart id", user?.cartId);
-      console.log("user id", user?.id);
-      console.log("cart id from usestate: ", cartId);
-      // add isloading here to the button, 
-      
-      const response = await fetch(`/api/users/${authUser?.id}/cart/${cartId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          productId: product?.id,
-          quantity: 1,
-        }),
-      });
-      
-      const data = await response.json();
-      console.log("add to cart data", data);
-      
-      toast.promise(Promise.resolve(response), {
-        success: {
-          title: "Product added to cart",
-          description: "Check your cart to proceed to checkout",
-        },
-        loading: { title: "Adding to cart", description: "Please wait..." },
-        error: { title: "Failed to add item to cart", description: "Please try again later" },
-      });
+    setIsLoading(true);
+    console.log("Cart id", user?.cartId);
+    console.log("user id", user?.id);
+
+    if(!user || !product || !user.cartId) {
+      console.error("User, product or cart not found");
       setIsLoading(false);
+      return;
     }
-  
+    
+    const response = await fetch(`/api/users/${user.id}/cart/${user.cartId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        productId: product?.id,
+        quantity: 1,
+      }),
+    });
+    
+    const data = await response.json();
+    console.log("add to cart data", data);
+    
+    toast.promise(Promise.resolve(response), {
+      success: {
+        title: "Product added to cart",
+        description: "Check your cart to proceed to checkout",
+      },
+      loading: { title: "Adding to cart", description: "Please wait..." },
+      error: { title: "Failed to add item to cart", description: "Please try again later" },
+    });
+    setIsLoading(false);
+  }
+
 
   return (
     <PageContainer>
-      {product ? (
+      {(!isLoading && product) ? (
         <SimpleGrid
           columns={{ base: 1, md: 2 }}
           spacing={10}
